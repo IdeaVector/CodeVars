5aeea025e5a3419049b4a75eb0a9004d8f31f6f9
morozzArt <morozz9846@gmail.com>
2019-09-28 23:08:48 +0300

    separated handler to separate module

---
+++ route/Handlers.go
@@ -0,0 +1,21 @@
+package Route
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	fmt.Print(r.RemoteAddr + "\n")
+	if err != nil {
+		log.Fatalf("Oops! Failed reading body of the request.\n %s", err)
+		http.Error(w, err.Error(), 500)
+	}
+
+	fmt.Print(string(body[:]))
+}
