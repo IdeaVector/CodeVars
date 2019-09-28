ffeb9b9e8911870cc7adaec8ae00829dd4e8dd98
TimurSirkin <timur_sirkin@mail.ru>
2019-09-28 22:46:12 +0300

    User model and interface changes - Add UserModel - Change UserInterface function's names

--- Interfaces/UserInterface.go
+++ Interfaces/UserInterface.go
@@ -1,8 +1,8 @@
 package Interfaces
 
 type UserInterface interface {
-	SetName(name string)
-	GetName() string
-	SetRemoteAddress(ip string)
-	GetRemoteAddress() string
+	setName(name string)
+	getName() string
+	setRemoteAddress(ip string)
+	getRemoteAddress() string
 }
---
+++ Models/UserModel.go
@@ -0,0 +1,22 @@
+package Models
+
+type UserModel struct {
+	name          string
+	remoteAddress string
+}
+
+func (user UserModel) getName() string {
+	return user.name
+}
+
+func (user UserModel) setName(name string) {
+	user.name = name
+}
+
+func (user UserModel) getRemoteAddress() string {
+	return user.remoteAddress
+}
+
+func (user UserModel) setRemoteAddress(address string) {
+	user.remoteAddress = address
+}
